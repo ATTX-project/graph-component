708980aa41dea905d2311f6a891f79dd7cde87ec
Stefan Negru <stefan.negru@helsinki.fi>
2017-01-13 01:02:54 +0200

    package refactor

--- gc-feature-tests/src/test/java/org/uh/attx/gc/graphcomponent/test/stepdefinitions/CronJobSteps.java
+++ gc-feature-tests/src/test/java/org/uh/attx/gc/graphcomponent/test/stepdefinitions/CronJobSteps.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.uh.attx.etl.graphcomponent.test.stepdefinitions;
+package org.uh.attx.gc.graphcomponent.test.stepdefinitions;
 
 import cucumber.api.java8.En;
 
