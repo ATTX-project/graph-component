6cbdb04c95e94f9d9319314e6332608db5e0e910
Stefan Negru <stefan.negru@helsinki.fi>
2017-01-27 10:52:01 +0200

    ATTX-project/graph-component#8 mock api

--- gm-API/src/gm_api/api/mapping.py
+++ gm-API/src/gm_api/api/mapping.py
@@ -1,24 +1,40 @@
 import falcon
 import json
+from random import randint, choice
+
+
+quotes = [
+    '"Kicking ass takes getting your ass kicked" ~ Jason Calacanis',
+    '"The perfect is the enemy of the good." ~ Voltaire',
+    '"In essence, if we want to direct our lives, we must take control of our\
+    consistent actions. It\'s not what we do once in a while that shapes our\
+    lives, but what we do consistently." ~ Tony Robbins'
+    ]
+
+mock_response = {'id': randint(0, 200), 'status': choice(quotes)}
 
 
 class Mapping(object):
     """Create Mapping class."""
 
-    def on_get(self, req, resp):
-        """Respond on GET request to map endpoint."""
-        resp.data = json.dumps({'message': 'Hello world!'})
-        resp.content_type = 'application/json'
-        resp.status = falcon.HTTP_200
-
-    def on_delete(self, req, resp):
-        """Respond on GET request to map endpoint."""
-        resp.data = json.dumps({'message': 'Hello world!'})
-        resp.content_type = 'application/json'
-        resp.status = falcon.HTTP_200
-
     def on_post(self, req, resp):
         """Respond on GET request to map endpoint."""
-        resp.data = json.dumps({'message': 'Hello world!'})
+        resp.data = json.dumps(mock_response)
+        resp.content_type = 'application/json'
+        resp.status = falcon.HTTP_202
+
+
+class MappingResource(object):
+    """Create Mapping Resource based on ID for retrieval."""
+
+    def on_get(self, req, resp, mapID):
+        """Respond on GET request to map endpoint."""
+        resp.data = json.dumps(mock_response)
+        resp.content_type = 'application/json'
+        resp.status = falcon.HTTP_200
+
+    def on_delete(self, req, resp, mapID):
+        """Respond on GET request to map endpoint."""
+        resp.data = json.dumps(mock_response)
         resp.content_type = 'application/json'
         resp.status = falcon.HTTP_200
--- gm-API/src/gm_api/app.py
+++ gm-API/src/gm_api/app.py
@@ -1,13 +1,14 @@
 import falcon
-from gm_api.api.mapping import Mapping
+from gm_api.api.mapping import Mapping, MappingResource
 from wsgiref import simple_server  # NOQA
 from gm_api.utils.logs import main_logger
 
 sample_resource = Mapping()
+resource_response = MappingResource()
 
 gm_app = falcon.API()
 gm_app.add_route('/map', sample_resource)
-gm_app.add_route('/map/{mapID}', sample_resource)
+gm_app.add_route('/map/{mapID}', resource_response)
 gm_app.add_route('/clusterids', sample_resource)
 gm_app.add_route('/links', sample_resource)
 
