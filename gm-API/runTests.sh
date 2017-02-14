29519f0b65adc6fce4bd776bb2e4ac570e2d3fa3
Stefan Negru <stefan.negru@helsinki.fi>
2017-02-14 15:19:55 +0200

    small change to the test report folders

--- gm-API/runTests.sh
+++ gm-API/runTests.sh
@@ -3,4 +3,4 @@ command -v source >/dev/null 2>&1 || {
   echo "I require source but it's not installed.  Aborting." >&2; exit 1;
 }
 source activate
-py.test tests --html=build/test-report/index.html --self-contained-html
+py.test tests --html=build/test-report/index.html
